6d2c60817cee9b3c862d34f3e2989a36dd2a0757
Milan Hommet <milan.hommet@protonmail.com>
2024-04-06 23:48:08 +0200

    changing bar color and ram script to show ram usage %

--- scripts/ram.sh
+++ scripts/ram.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-mem="$(free -h | awk '/^Mem/ { print $3 }' | sed s/i//g)"
-echo -e "$mem"
+# Get total memory and used memory
+total_mem=$(free -m | awk 'NR==2 {print $2}')
+used_mem=$(free -m | awk 'NR==2 {print $3}')
+
+# Calculate memory usage percentage
+mem_usage=$((used_mem * 100 / total_mem))
+
+echo "$mem_usage %"
+
 
